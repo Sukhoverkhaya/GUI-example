6f8f3d1c9c0668cb3c053d6b0e88cda5b04bba19
Katerina Sukhoverkhaya <katerina.sukhoverkhaya@gmail.com>
2023-05-11 19:46:27 +0300

    добавлены комментарии

--- interactive_cursor_example.jl
+++ interactive_cursor_example.jl
@@ -14,37 +14,41 @@ using .Renderer
 include("src/readfiles.jl")
 include("src/refmkpguifunctions.jl") 
 
-const BINPATH = "data/bin/Noise Base"
-const BASEPATH = "data/raw mkp/Noise base"
-const REFPATH = "data/new mkp/Noise base"
+const BINPATH = "data/bin/Noise Base"       # путь к папке с бинарями (должна существовать)
+const BASEPATH = "data/raw mkp/Noise base"  # путь к папке с исходной разметкой (должна существовать)
+const REFPATH = "data/new mkp/Noise base"   # путь к папке с изменённой разметкой (может не существовать)
 
-mkpath(REFPATH)
+mkpath(REFPATH) # создаём путь к папке с изменённой разметкой, если его не было
 
-const FILENAMES = readdir(BASEPATH)
+const FILENAMES = readdir(BASEPATH) # имена файлов базы
 
-const SELECTED = Dict{String, Any}()
-SELECTED["file"] = 1
+const SELECTED = Dict{String, Any}() # здесь храним информацию о выбранной записи, именах измерений в ней и выбранном измерении
+SELECTED["file"] = 1    # задаём значения по умолчанию для старта 
 SELECTED["record"] = 1
 SELECTED["recordnames"] = readdir("$BASEPATH/$(FILENAMES[SELECTED["file"]])")
 
-const RECORDDATA = Dict{String, Any}()
+const RECORDDATA = Dict{String, Any}() # здесь храним прочитанную из выбранной записи информацию
 
-mutable struct PlotBounds   # Данные границ сегмента, рабочей зоны и АД (рз и ад - на накачке и на спуске)
+mutable struct PlotBounds   # данные границ сегмента, рабочей зоны и АД (рз и ад - на накачке и на спуске)
     AD::NamedTuple{(:pump, :desc), NTuple{2, Bounds}}
     workreg::NamedTuple{(:pump, :desc), NTuple{2, Bounds}}
     segbounds::Bounds
 end
 
-mutable struct PlotState
+mutable struct PlotState    # границы по осям для графика
     xlim::NamedTuple{(:min, :max), NTuple{2, Float32}}
     ylim::NamedTuple{(:min, :max), NTuple{2, Float32}}
 end
 
-const PLOTSTATE = Dict{String, Any}()
+const PLOTSTATE = Dict{String, Any}() # здесь храним состояние графика
 
-function read_record()
+# чтение записи
+function read_record() 
+
+    # чтение сигнала
     signals, fs, _, _ = readbin("$BINPATH/$(FILENAMES[SELECTED["file"]])")
 
+    # чтение разметки (в приоритете - ранее изменённая. если такой не было - берём исходную)
     tone_mkp =  try ReadRefMkp("$REFPATH/$(FILENAMES[SELECTED["file"]])/$(SELECTED["record"])/tone.csv") 
                 catch e 
                     ReadRefMkp("$BASEPATH/$(FILENAMES[SELECTED["file"]])/$(SELECTED["record"])/tone.csv") 
@@ -55,7 +59,7 @@ function read_record()
             end
 
     plotbounds = PlotBounds((pump = bnds.iad.pump, desc = bnds.iad.desc), (pump = bnds.iwz.pump, desc = bnds.iwz.desc), bnds.segm)
-    tone = signals.Tone[bnds.segm.ibeg:bnds.segm.iend] 
+    tone = signals.Tone[bnds.segm.ibeg:bnds.segm.iend] # берём из записи только сегмент, соответствующий выбранному измерению
     pres = signals.Pres[bnds.segm.ibeg:bnds.segm.iend] 
 
     RECORDDATA["tone"] = tone
@@ -68,13 +72,17 @@ function read_record()
     PLOTSTATE["flag"] = ImGuiCond_Once
 end
 
-read_record()
+read_record() # читаем первый раз для запуска
 
+# таблицы с именами файлов и именами измерений в выбранном файле
 function FilesTable()
+
+    # заголовки
     CImGui.TextColored(ImVec4(0.45, 0.7, 0.80, 1.00), "Имена файлов базы")
     CImGui.SameLine(CImGui.GetWindowContentRegionWidth()*0.52)
     CImGui.TextColored(ImVec4(0.45, 0.7, 0.80, 1.00), "Номера измерений в выбранной записи")
 
+    # таблица с именами файлов
     CImGui.BeginChild("##filenames_scrollingregion", ImVec2(CImGui.GetWindowContentRegionWidth()*0.5, CImGui.GetWindowHeight()*0.7))
         CImGui.Columns(1, "Имена файлов")
         CImGui.Separator()
@@ -92,6 +100,7 @@ function FilesTable()
 
     CImGui.EndChild()
 
+    # таблица с именами измерений
     CImGui.SameLine(CImGui.GetWindowContentRegionWidth()*0.52)
     CImGui.BeginChild("##records_scrollingregion", ImVec2(CImGui.GetWindowContentRegionWidth(), CImGui.GetWindowHeight()*0.7))
         CImGui.Columns(1, "Номера измерений")
@@ -111,27 +120,30 @@ function FilesTable()
     CImGui.EndChild()
 end
 
+# окно меню
 function MenuWindow()
     CImGui.Begin("Меню")
         FilesTable()
     CImGui.End()
 end
 
+# захват курсора - выбираем, какая из четырёх границ ближе к точке, в которой случилось первое нажатие на график, чтобы до отпускания ЛКМ перемещать только эту границу
 function CatchCursor()
-    pt = ImPlot.GetPlotMousePos()
-    xpos = pt.x
+    pt = ImPlot.GetPlotMousePos() # позиция мыши
+    xpos = pt.x                   # позиция мыши по оси абсцисс
 
-    bnd = PLOTSTATE["bounds"]
+    bnd = PLOTSTATE["bounds"]     # берём актуальные границы
     p1, p2, p3, p4 = bnd.AD.pump.ibeg, bnd.AD.pump.iend, bnd.AD.desc.ibeg, bnd.AD.desc.iend
-    crsr = [p1, p2, p3, p4] .* 1.0
+    crsr = [p1, p2, p3, p4] .* 1.0 # формируем из их вектор и переводим во float64
 
-    moveind = ([abs(x - xpos) for x in crsr] |> findmin)[2]
-    crsr[moveind] = xpos
+    moveind = ([abs(x - xpos) for x in crsr] |> findmin)[2] # ищем индекс курсора, ближайшего к позиции мыши
 
-    PLOTSTATE["active cursor ind"] = moveind
+    PLOTSTATE["active cursor ind"] = moveind # сохраняем индекс курсора
 end
 
+# перемещение захваченного курсора
 function MoveCursor()
+    # ниже всё как в CatchCursor()
     pt = ImPlot.GetPlotMousePos()
     xpos = pt.x
 
@@ -139,12 +151,14 @@ function MoveCursor()
     p1, p2, p3, p4 = bnd.AD.pump.ibeg, bnd.AD.pump.iend, bnd.AD.desc.ibeg, bnd.AD.desc.iend
     crsr = [p1, p2, p3, p4] .* 1.0
 
+    # читаем индекс захваченного курсора и меяем его позицию на позицию мыши
     moveind = PLOTSTATE["active cursor ind"]
-    crsr[moveind] = xpos
+    crsr[moveind] = xpos 
 
     PLOTSTATE["bounds"].AD = (pump = Bounds(crsr[1], crsr[2]), desc = Bounds(crsr[3], crsr[4]))
 end
 
+# рисование графика
 function MakePlot()
 
     tone = RECORDDATA["tone"]
@@ -156,34 +170,36 @@ function MakePlot()
     mkp = RECORDDATA["markup"]
     peaks = [x.pos for x in mkp]
 
-    CImGui.PushID("$(SELECTED["file"])$(SELECTED["record"])")
+    CImGui.PushID("$(SELECTED["file"])$(SELECTED["record"])") # закрепляем индивидуальный айдишник за каждым графиком, чтобы удобнее было работать с масштабами
         ImPlot.SetNextPlotLimits(lims.xlim.min, lims.xlim.max, lims.ylim.min, lims.ylim.max, flag)
         if ImPlot.BeginPlot("Тоны", C_NULL, C_NULL, ImVec2(CImGui.GetWindowContentRegionMax().x, CImGui.GetWindowContentRegionMax().y*0.8),
             x_flags = ImPlotAxisFlags_NoGridLines | ImPlotAxisFlags_NoDecorations, y_flags = ImPlotAxisFlags_NoGridLines)
 
+            # рисуем сигнал тонов, его разметку и границы сад и дад
             ImPlot.PlotLine(tone, label_id = "Сигнал тонов")
             ImPlot.PlotScatter(peaks, tone[peaks])
 
             ImPlot.PlotVLines("САД и ДАД на накачке", [bnd.AD.pump.ibeg, bnd.AD.pump.iend], 2)
             ImPlot.PlotVLines("САД и ДАД на спуске", [bnd.AD.desc.ibeg, bnd.AD.desc.iend], 2)
 
-            if ImPlot.IsPlotHovered() && CImGui.IsMouseClicked(0)
-                PLOTSTATE["flag"] = ImGuiCond_Always
-                CatchCursor()
-            elseif ImPlot.IsPlotHovered() && CImGui.IsMouseDown(0)
-                PLOTSTATE["flag"] = ImGuiCond_Always
-                MoveCursor()
+            # флаг закрепления масштаба и перемещение курсоров зависят от действий мыши
+            if ImPlot.IsPlotHovered() && CImGui.IsMouseClicked(0) # если навелись на график и нажали ЛКМ (при наведении на график и долгом нажатии ЛКМ этот флаг загорается первым и только один раз)
+                PLOTSTATE["flag"] = ImGuiCond_Always # закрепляем масштаб
+                CatchCursor()                        # определяем, какой курсор собираемся перемещать
+            elseif ImPlot.IsPlotHovered() && CImGui.IsMouseDown(0) # навелись на график и зажали (и держим) ЛКМ (флаг горит всё время, пока зажата ЛКМ)
+                PLOTSTATE["flag"] = ImGuiCond_Always # закрепляем масштаб
+                MoveCursor()                         # двигаем выбранный в предыдущем условии курсор
             else
-                PLOTSTATE["flag"] = ImGuiCond_Once
+                PLOTSTATE["flag"] = ImGuiCond_Once   # в противном случае открепляем масштаб
             end
 
-            limits = ImPlot.GetPlotLimits()
+            limits = ImPlot.GetPlotLimits()       # сохраняем текущие лимиты графика по осям
             x1, x2 = limits.X.Min, limits.X.Max
             y1, y2 = limits.Y.Min, limits.Y.Max
 
             PLOTSTATE["limits"] = PlotState((min = x1, max = x2), (min = y1, max = y2))
 
-            if CImGui.IsMouseDoubleClicked(0)
+            if ImPlot.IsPlotHovered() && CCImGui.IsMouseDoubleClicked(0) # при двойном нажатии ЛКМ на графике возвращаем его исходный масштаб
                 PLOTSTATE["limits"] = PlotState((min = 1, max = lastindex(tone)), (min = minimum(tone)*1.2, max = maximum(tone)*1.2))
             end
 
@@ -199,7 +215,8 @@ function to_int(obj::Bounds)
     return obj
 end
 
-function SaveButton()
+# сохранение изменённой разметки
+function SaveButton() 
     if CImGui.Button("Сохранить границы")
         bnd = PLOTSTATE["bounds"]
         segbounds = bnd.segbounds
@@ -214,6 +231,7 @@ function SaveButton()
     end
 end
 
+# окно с графком
 function PlotWindow()
     CImGui.Begin("График")
         SaveButton()
@@ -221,6 +239,7 @@ function PlotWindow()
     CImGui.End()
 end
 
+# собираем все окна
 function ui()
     MenuWindow()
     PlotWindow()
